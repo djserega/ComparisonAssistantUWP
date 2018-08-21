b048a41bbaa39e3fc293761b0ff424b7cb7600e1
djserega <galyuk.sergey@gmail.com>
2018-08-21 13:57:38 +0300

    add class dialogs

---
+++ src/ComparisonAssistant/Dialogs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace ComparisonAssistant
+{
+    internal static class Dialogs
+    {
+        internal static async void Show(string message)
+        {
+            ContentDialog contentDialog = new ContentDialog()
+            {
+                Title = "Помощник сравнения",
+                Content = message,
+                CloseButtonText = "ОК"
+            };
+            await contentDialog.ShowAsync();
+        }
+    }
+}
