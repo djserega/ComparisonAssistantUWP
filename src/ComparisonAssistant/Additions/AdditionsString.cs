ca2af13bafbbf2500714b80927c5117964263d83
dj_serega <galyuk.sergey@gmail.com>
2018-08-25 08:20:34 +0300

    added Additions\AdditionsString

---
+++ src/ComparisonAssistant/Additions/AdditionsString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparisonAssistant.Additions
+{
+    public static class AdditionsString
+    {
+        public static string RemoveStartText(this string text, string find)
+        {
+            if (text.StartsWith(find))
+                return text.Substring(find.Length);
+            else
+                return text;
+        }
+    }
+}
